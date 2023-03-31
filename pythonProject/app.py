2da6d1f0d6f05f9da8d90824711296434d8ba83f
kevin-trochon <trochonkevin@gmail.com>
2023-03-31 11:32:01 +1100

    finalisation du client python

--- pythonProject/app.py
+++ pythonProject/app.py
@@ -5,19 +5,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #dictionnaire de data
-    data = {'user':'kevin','machine':'windows'}
-    # affichage
-    return render_template('index.html',title='Calcule',data=data)
+    return render_template('index.html',title='Calcule K Means')
 
-@app.route('/traitement',methods=["POST","GET"])
+@app.route('/resultat',methods=["POST","GET"])
 def calcul_k_moyen():
     path = request.form.get('file')
     nb_cluster = request.form.get('nb_cluster')
     wsdl = 'http://localhost:8685/?wsdl'
     client = zeep.Client(wsdl=wsdl)
     data = client.service.calculKMoyen(path.replace('\\','\\\\'), nb_cluster)
-    return render_template('resultat.html',title='Resultat',data=data)
+    value = []
+    for item in data:
+        value.append(item['item'])
+    return render_template('resultat.html',title='Resultat',salaires=value)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=9999,debug=True)
+    app.run(host='127.0.0.1',port=9899,debug=True)
